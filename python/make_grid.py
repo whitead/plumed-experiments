f4a4c34c7516b4c77bc5bf5bf6c43dfc8200e057
Andrew White <white.d.andrew@gmail.com>
2014-01-18 21:36:29 -0600

    Improved grid construction and added new test images. Now can resize grid.

--- python/make_grid.py
+++ python/make_grid.py
@@ -42,58 +42,64 @@ class Grid:
 
     def __init__(self):
         """Create a grid. Add individual CVs using the add_cv method"""
-        self.nbins = []
         self.min = []
         self.max = []
         self.min = []
         self.dx = []
         self.types = []
-        self.nbins = []     
         self.periodic = []
         self.pot = None
-        self.ncv = 0
 
-    def add_cv(self, name, dx, min, max, periodic=False):
-        if(self.pot is not None):
-            raise ValueError("You must add all CVs before adding values")
-        self.nbins.append(int(ceil((max - min) / dx)))
+    @property
+    def nbins(self):
+        if(self.pot is None):
+            return ()
+        return np.shape(self.pot)
+    
+    @property
+    def ncv(self):
+        if(self.pot is None):
+            return 0
+        return len(np.shape(self.pot))
+
+    def add_cv(self, name, min, max, bin_number, periodic=False):
+        self.dx.append(float(max - min) / bin_number)
         self.min.append(min)
         self.max.append(max)
         self.periodic.append(periodic)
         self.types.append(Grid.cv_type_map[name])
-        self.dx.append(float(dx))
-        self.ncv += 1
+        if(self.pot is None):
+            self.pot = np.zeros(bin_number)
+        else:
+            self.pot = np.resize(self.pot, self.nbins + (int(bin_number),))
+        
+    def to_index(self, x, i):
+        return max(0, min(self.nbins[i] - 1, int(floor( (x - self.min[i]) / self.dx[i]) )))
                
     def add_value(self, x, v):
-        if(self.pot is None):
-            self.pot = np.zeros(self.nbins)
         if(len(x) != self.ncv):
             raise ValueError("Dimension of given x vector does not match grid dimension!")
         index = [0 for xi in x]
         for i, xi in enumerate(x):
             assert xi >= self.min[i] and xi <= self.max[i],"Mesh point is not within grid dimension {}: {}, [{}, {}]".format(i, xi, self.min[i], self.max[i])
-            index[i] = max(0, min(self.nbins[i] - 1, int(floor( (xi - self.min[i]) / self.dx[i]) )))
+            index[i] = self.to_index(xi, i)
         self.pot[tuple(index)] += v
 
     def set_value(self, x, v):
-        if(self.pot is None):
-            self.pot = np.zeros(self.nbins)
         if(len(x) != self.ncv):
             raise ValueError("Dimension of given x vector does not match grid dimension!")
         index = [0 for xi in x]
         for i, xi in enumerate(x):
             assert xi >= self.min[i] and xi <= self.max[i],"Mesh point is not within grid dimension {}: {}, [{}, {}]".format(i, xi, self.min[i], self.max[i])
-            index[i] = max(0, min(self.nbins[i] - 1, int(floor( (xi - self.min[i]) / self.dx[i]) )))
+            index[i] = self.to_index(xi, i)
         self.pot[tuple(index)] = v
 
             
     def get_value(self, x, v):
-        if(self.pot is None):
-            self.pot = np.zeros(self.nbins)
         index = [0 for xi in x]
         for i, xi in enumerate(x):
             assert xi >= self.min[i] and xi <= self.max[i],"Mesh point is not within grid dimension {}: {}, [{}, {}]".format(i, xi, self.min[i], self.max[i])
-            index[i] = max(0, min(self.nbins[i] - 1, int(floor( (xi - self.min[i]) / self.dx[i]) )))
+            index[i] = self.to_index(xi, i)
         return self.pot[tuple(index)]
 
 
@@ -108,7 +114,12 @@ class Grid:
         array_copy = copy.copy(array)
         array_copy.insert(0, element)
         return array_copy
-        
+
+    def resize(self, new_shape):
+        from scipy.ndimage.interpolation import zoom
+        zoom_factor = np.array(new_shape, dtype='float') / self.nbins
+        self.pot = zoom(self.pot, zoom_factor, prefilter=True, mode='nearest')
+                        
     def _enumerate_grid(self, fxn, dim=None, indices=[]):
         if(dim is None):
             dim = self.ncv - 1
@@ -124,8 +135,8 @@ class Grid:
 
     def _print_grid(self, indices, output):
         for i,j in enumerate(indices):
-            output.write('{0:05} '.format(j * self.dx[i] + self.min[i]))        
-        output.write('{0:05}\n'.format(self.pot[tuple(indices)]))
+            output.write('{:8} '.format(j * self.dx[i] + self.min[i]))        
+        output.write('{:08}\n'.format(self.pot[tuple(indices)]))
             
 
     def write(self, output):
@@ -134,43 +145,58 @@ class Grid:
         self._print_header_array('TYPE', self.types, output)
 
         #Some kind of weird Plumed convention
-        mod_bins = copy.copy(self.nbins)        
+        mod_bins = list(self.nbins)        
         mod_max = copy.copy(self.max)
         for i,p in enumerate(self.periodic):
             mod_bins[i] -= 1 if p else 0
-            mod_max[i] -= dx[i] if p else 0                
+            mod_max[i] -= 2 * self.dx[i] if p else self.dx[i]
 
-        self._print_header_array('BIN', mod_bins, output)
+        self._print_header_array('BIN', np.array(mod_bins) - 1, output)
         self._print_header_array('MIN', self.min, output)
         self._print_header_array('MAX', mod_max, output)
-        self._print_header_array('PBC', [0 if x else 1 for x in self.periodic], output)
+        self._print_header_array('PBC', [1 if x else 0 for x in self.periodic], output)
         self._enumerate_grid(lambda x: self._print_grid(x, output))
         
 
     def add_png_to_grid(self, filename):
         if(self.ncv != 2):
-            raise ValueError("This method only makes sense on 2D grids")
+            raise ValueError("This method only makes sense on 2D grids. Grid is currently {} dimension".format(self.ncv))
         from pylab import imread, imshow, gray, mean
         a = imread(filename) # read to RGB file
         gray_scale = mean(a,2) # convert to grayscale
+        #replace zeros with lowest P
+        #non-zero gray
+        non_zero_max = np.max(gray_scale[np.where(gray_scale) > 0])
+        print non_zero_max
+        gray_scale[np.where(gray_scale == 0)] = non_zero_max
         csum = np.sum(gray_scale)        
-        if(np.shape(gray_scale)[0] != self.nbins[0] or np.shape(gray_scale)[1] != self.nbins[1]):
-            raise ValueError("Your image must be exactly the same number of pixels as your bin number: {}".format(self.nbins))
-
-        if(self.pot is None):
-            self.pot = np.zeros(self.nbins)
-        for i in range(np.shape(gray_scale)[0]):
-            for j in range(np.shape(gray_scale)[1]):
-                self.pot[i,j] += log(gray_scale[i,j]) - log(csum)
+        self.resize(np.shape(gray_scale))
+        self.pot += np.log(gray_scale) - log(csum)    
                                                                 
 
 def test():
     import sys
+    import matplotlib.pyplot as plt
     g = Grid()
-    g.add_cv("Distance", 0.5, 0, 16, True)
-    g.add_cv("Distance", 0.5, 0, 16, True)
+    g.add_cv("Distance", 0, 8, 16, True)
+    g.add_cv("Distance", 0, 8, 16, True)
     g.add_png_to_grid("circle.png")
-    g.write(sys.stdout)
+    g.resize((600,600))
+    plt.imshow(np.exp(g.pot), interpolation='none', cmap='binary')
+    plt.savefig("circle_out.png")
+
+    g.resize((8,8))
+#    g.write(sys.stdout)    
+
+
+    g = Grid()
+    g.add_cv("Distance", 0, 10, 128, True)
+    g.add_cv("Distance", 0, 10, 128, True)
+    g.add_png_to_grid("uc_shield.png")
+    g.resize((128,128))
+    plt.imshow(np.exp(g.pot), interpolation='none', cmap='binary')
+    plt.savefig("uc_out.png")
+
 
 if __name__ == "__main__":
     test()
