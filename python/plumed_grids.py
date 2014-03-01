ef2dd96bed12da50009041ce9346b17f1674accf
Andrew White <white.d.andrew@gmail.com>
2014-02-28 18:57:51 -0600

    Merge
    Conflicts:
    	python/plumed_grids.py

--- python/plumed_grids.py
+++ python/plumed_grids.py
@@ -343,6 +343,15 @@ class Grid(object):
         plt.colorbar()
         plt.savefig(filename)
 
+    def remove_target_bias(self, target_filename, bias_factor=2):
+        self.pot *= (bias_factor) / (bias_factor - 1)
+        t = Grid()
+        t.read_plumed_grid(target_filename)
+        t.pot *= -1.0
+        self.add(t)
+        self.pot -= np.min(self.pot)
+        
+        
 
     def plot_2d_region(self, filename, *region_functions):
         assert self.dims >= 2
@@ -390,7 +399,8 @@ class Grid(object):
 
 
         if(self.meshgrid is None):
-            self.meshgrid = np.meshgrid(*[np.arange(min, max, dx) for min,max,dx in zip(self.min, self.max, self.dx)], indexing='ij')
+#            self.meshgrid = np.meshgrid(*[np.arange(min, max, dx) for min,max,dx in zip(self.min, self.max, self.dx)], indexing='ij')
+            self.meshgrid = np.meshgrid(*[np.arange(min, max, dx) for min,max,dx in zip(self.min, self.max, self.dx)])
         for x in np.nditer(self.meshgrid):
             indexs = self.np_to_index(x)
             if(not region_function(x)):
@@ -485,26 +495,6 @@ def load_plumed_grid(filename):
     g = Grid()
     g.read_plumed_grid(filename)
     g.plot_2d("plot.png")
-
-def plot_free_energy(bias_grid, target, boltzmann_factor=2, bias_factor=1):
-    g = Grid()
-    g.read_plumed_grid(bias_grid)
-    g.pot *= (bias_factor) / (bias_factor - 1)
-
-    t = Grid()
-    t.read_plumed_grid(target)
-    t.pot *= boltzmann_factor    
-    t.normalize()
-
-    g.write(open("bias_before_add.grid", "w"))
-    t.write(open("target_before_add.grid", "w"))
-    g.add(t)
-    g.pot *= -1.
-    g.plot_2d("free_energy.png")
-    g.write(open("free_energy.grid", "w"))
-
-    return 
-    
 
 if __name__ == "__main__":
     #test()
