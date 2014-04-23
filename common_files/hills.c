4a91725c0eebf542ce7a7dfe206c861d5a1e4cde
Andrew White <white.d.andrew@gmail.com>
2014-04-23 15:11:22 -0500

    Fixed subtle single precision bug

--- common_files/hills.c
+++ common_files/hills.c
@@ -882,10 +882,11 @@ void PREFIX grid_addhills(struct grid_s *grid, real ww, real* ss, real* delta,in
 
     flag = 0;
     for(j = 0; j < ncv; j++) {
-      xx[j] = ss[grid->index[j]] - grid->minilbox[j] + grid->dx[j] * grid->one2multi[i][j];  
-      if(grid->period[j]) xx[j] -= grid->lbox[j] * rint(xx[j] / grid->lbox[j]);
-      if((xx[j] < grid->min[j] || xx[j] >= grid->max[j]) && !grid->period[j])  flag = 1;
-      index_nd[j] = floor((xx[j] - grid->min[j]) / grid->dx[j]);
+      xx[j] = ss[grid->index[j]] - grid->minilbox[j] + grid->dx[j] * grid->one2multi[i][j];
+      if(grid->period[j]) xx[j] -= grid->lbox[j] * rint(xx[j]/grid->lbox[j]);
+      index_nd[j] = floor((xx[j]-grid->min[j])/grid->dx[j]);
+      //with single precision, it is possible that xx - min = 2 * min if xx[j] is very close to min
+      if(index_nd[j]<0 || index_nd[j] >=grid->bin[j])  flag=1;
     }
     if(flag == 1) continue; // out of grid 
 
