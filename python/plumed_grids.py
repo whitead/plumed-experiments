c570efb0ddefb0be4c909c6ef2ecd93ea56b987a
Andrew White <white.d.andrew@gmail.com>
2015-04-24 01:05:29 -0400

    Improved png support

--- python/plumed_grids.py
+++ python/plumed_grids.py
@@ -719,12 +719,9 @@ class Grid(object):
             gray_scale = 1 - gray_scale
             
         gray_scale = gray_scale.astype(np.float64)
-        gray_scale = np.rot90(gray_scale)
-        #too_small = exp(-10)
-        #gray_scale[np.where(gray_scale < too_small)] = too_small        
-        #self.pot += np.log(gray_scale)
+        gray_scale = gray_scale.transpose()
         old_bins = self.nbins
-        self.set_bin_number(np.shape(gray_scale))
+        self.set_grid_point_number(np.shape(gray_scale))
         self.pot += gray_scale
         self.normalize()
         self.set_bin_number(old_bins)
